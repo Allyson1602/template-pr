69a43028db80de2e7a73f89f0cffa09029e89a86
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-06-21 17:33:03 -0300

    novo comite

--- app/page.tsx
+++ app/page.tsx
@@ -107,6 +107,8 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <h1>Criação de branch para testar template. Olá mundo</h1>
       </div>
     </main>
   );
